37e52cc65178337d9133f32a477bef2565bf81c9
ParamiJayasinghe <apurvajayasinghe@gmail.com>
2024-07-11 21:01:16 +0530

    Backend of application done

---
+++ src/main/java/com/example/SpringMongoProject/Entity/Student.java
@@ -0,0 +1,67 @@
+package com.example.SpringMongoProject.Entity;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document(collection = "students")
+public class Student {
+
+    @Id
+    private String _id;
+    private String studentname;
+    private String studentadress;
+    private String mobile;
+
+
+    public Student(String _id, String studentname, String studentadress, String mobile) {
+        this._id = _id;
+        this.studentname = studentname;
+        this.studentadress = studentadress;
+        this.mobile = mobile;
+    }
+
+    public Student() {
+    }
+
+    public String get_id() {
+        return _id;
+    }
+
+    public void set_id(String _id) {
+        this._id = _id;
+    }
+
+    public String getStudentname() {
+        return studentname;
+    }
+
+    public void setStudentname(String studentname) {
+        this.studentname = studentname;
+    }
+
+    public String getStudentadress() {
+        return studentadress;
+    }
+
+    public void setStudentadress(String studentadress) {
+        this.studentadress = studentadress;
+    }
+
+    public String getMobile() {
+        return mobile;
+    }
+
+    public void setMobile(String mobile) {
+        this.mobile = mobile;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "_id='" + _id + '\'' +
+                ", studentname='" + studentname + '\'' +
+                ", studentadress='" + studentadress + '\'' +
+                ", mobile='" + mobile + '\'' +
+                '}';
+    }
+}
